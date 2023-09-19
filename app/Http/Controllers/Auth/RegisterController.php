dcf1c3accb9222d2f8eec1044ddc3013aa021261
Egor Danilov <egordanilov970@gmail.com>
2023-09-19 10:26:51 +0300

    Added registration form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,4 +10,21 @@ class RegisterController extends Controller
     public function index() {
         return view('auth.register');
     }
+
+    public function store(Request $request) {
+        
+        //validate
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed'
+        ]);
+        //create user
+        dd($request);
+        //login
+
+        //redirect
+
+    }
+
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-    Register
+<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-6/12 mx-auto mt-8" method="POST" action="{{route('register')}}">
+    @csrf
+    
+    <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+            Name
+        </label>
+        <input class="@error('name') border-red-500 @enderror shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Name" value="{{old('name')}}">
+        @error('name')
+            <div class="text-red-500">{{$message}}</div>
+        @enderror
+    </div>
+    
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+          Email
+      </label>
+      <input class="@error('email') border-red-500 @enderror shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" name="email" placeholder="Email" value="{{old('email')}}">
+        @error('email')
+            <div class="text-red-500">{{$message}}</div>
+        @enderror
+    </div>
+
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="@error('password') border-red-500 @enderror shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" placeholder="Your password">
+        @error('password')
+            <div class="text-red-500">{{$message}}</div>
+        @enderror
+    </div>
+
+    <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
+          Confirm Password
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password_confirmation" type="password" name="password_confirmation" placeholder="Confirm password">
+      </div>
+
+
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
+        Register
+      </button>
+    </div>
+  </form>
 @endsection
